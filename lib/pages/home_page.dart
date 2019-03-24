6ac6cd14662332fbdb15a3c7065ec7769dabfddd
yangcs2016 <davidyangcs2012@gmail.com>
2019-03-24 18:29:36 +0800

    增加主页导航栏

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../services/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
   final Widget child;
@@ -25,9 +26,11 @@ class _HomePageState extends State<HomePage> {
           if(snapshot.hasData){
             var data=json.decode(snapshot.data.toString());
             List<Map> swiper=(data['data']['slides'] as List).cast();
+            List<Map> navigatorList=(data['data']['category'] as List).cast();
             return Column(
               children:<Widget>[
-                SwiperDiy(swiperDataList: swiper,)
+                SwiperDiy(swiperDataList: swiper),
+                TopNavigator(navigatorList:navigatorList)
                 ],              
             );
           } else {
@@ -49,17 +52,53 @@ class SwiperDiy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 333,
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
       child: Swiper(
         itemBuilder: (BuildContext context,int index){
           return Container(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.network("${swiperDataList[index]['image']}",fit: BoxFit.fitHeight,),
+            //padding: const EdgeInsets.all(4.0),
+            child: Image.network("${swiperDataList[index]['image']}",fit: BoxFit.fill,),
           );          
         },
         itemCount: swiperDataList.length,
         pagination: SwiperPagination(),
         autoplay: true,
+      ),
+    );
+  }
+}
+
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+
+  TopNavigator({Key key, this.navigatorList}) : super(key: key);
+
+  Widget _gridViewItemUI(BuildContext context,item){
+    return InkWell(
+      onTap: (){print('taped');},
+      child: Column(
+        children: <Widget>[
+          Image.network(item['image'],width:ScreenUtil().setWidth(95)),
+          Text(item['mallCategoryName'])
+        ],
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    if(this.navigatorList.length>10){
+      this.navigatorList.removeRange(10,this.navigatorList.length);
+    }
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding: const EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: const EdgeInsets.all(5.0),
+        children: navigatorList.map((item){
+          return _gridViewItemUI(context, item);
+        }).toList(),
       ),
     );
   }
