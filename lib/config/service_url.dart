3b27acc712f2ef4032e302f8a98d7396fad98e9a
杨春松 <yangchunsong@zpmc.com>
2019-04-15 18:06:40 +0800

    增加商品推荐

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -1,4 +1,4 @@
-const serviceUrl='http://test.baixingliangfan.cn/baixing/';
+const serviceUrl='http://v.jspang.com:8088/baixing/';
 const servicePath={
   //商店首页内容url路径
   'homePageContent':serviceUrl+'wxmini/homePageContent',
